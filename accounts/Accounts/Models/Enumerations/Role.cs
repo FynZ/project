2b070e9caa10b10523260fe28c53ebffa22c664f
Fynzie <FynZ@Hotmail.Fr>
2019-04-06 13:14:44 +0200

    Rest api overhaul (#2)
    * c# api overhaul first part and some legacy try for gateway api
    
    * now working with swagger, elasticsearch and some shit
    
    * renamed solution + separated public/private key functions
    
    * Resharper warning cleaning & renaming
    
    * added sql folder with creation/useful scripts
    
    * now store user in database (still keeping commented in memory implementation)

---
+++ accounts/Accounts/Models/Enumerations/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounts.Models.Enumerations
+{
+    public enum Role
+    {
+        User = 1,
+        Admin = 2
+    }
+}
