b514aa12c28acb00d4943fcaef65c9dc09b724e6
vdebote <Fynz@Hotmail.Fr>
2019-07-22 16:33:54 +0200

    hard coded exceptions for auth interceptor (ugly)

--- src/front/src/app/interceptors/auth-interceptor.ts
+++ src/front/src/app/interceptors/auth-interceptor.ts
@@ -6,6 +6,25 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor
 {
+    // for an unknown reason response body are duplicated when adding token
+    // for an endpoint that does not need them
+    // and i'm unable to find out why
+    // so i'm hard coding skipping those endpoints
+    private exceptions = [
+        'http://localhost:80/health',
+        'http://localhost:80/service-discovery/health',
+        'http://localhost:80/news/actuator/health',
+        'http://localhost:80/ressources/health',
+        'http://localhost:80/auth/health',
+        'http://localhost:80/monsters/health',
+        'http://localhost:80/trading/health',
+        'http://localhost:80/news/news/1',
+        'http://localhost:80/news/news/4',
+        'http://localhost:80/news/news/5',
+        'http://localhost:80/news/news/6',
+        'http://localhost:80/news/news/7',
+    ];
+
     constructor(private authService: AuthService)
     {
     }
@@ -14,6 +33,12 @@ export class AuthInterceptor implements HttpInterceptor
     {
         if (this.authService.isAuthenticated())
         {
+            if (req.method == "GET" && this.exceptions.includes(req.url))
+            {
+                console.log(`Intercepted and no token exception request, skipping for ${req.url}`);
+                return next.handle(req);
+            }
+
             const token = this.authService.getToken();
 
             // console.log('token:');
@@ -23,10 +48,14 @@ export class AuthInterceptor implements HttpInterceptor
                 headers: req.headers.set('Authorization', 'Bearer ' + token)
             });
 
+            console.log(`Added auth token for ${req.url}`);
+
             return next.handle(cloned);
         }
         else
         {
+            console.log(`No token to add for ${req.url}`);
+
             return next.handle(req);
         }
     }
